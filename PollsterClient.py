283d81c4150f6cf0200c8f140463763f26f3137e
Sidharth Naik <sinaik@infinera.com>
2019-08-20 16:24:35 -0700

    removed the index.html etc

--- PollsterClient.py
+++ PollsterClient.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def project_run():
-    return render_template('index.html')
+    return render_template('national_poll_panel.html')
 
 
 if __name__ == "__main__":
